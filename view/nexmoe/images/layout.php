6c9f14de1ce26ff3279af49d0dfbc8a7b74b9cc5
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2019-03-27 11:55:58 +0800

    fix erro
    修复前台不显示的问题

--- view/nexmoe/images/layout.php
+++ view/nexmoe/images/layout.php
@@ -4,8 +4,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
 	<title><?php e($title.' - '.config('site_name'));?></title>
-	<meta name="keywords" content="佰阅部落baiyue.one">
-	<meta name="author" content="佰阅部落baiyue.one">>
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/css/mdui.min.css">
 	<link rel="stylesheet" href="https://cloud.i.chainwon.com/data/User/admin/home/css/main.css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
